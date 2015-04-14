e0b196d4ad930126958e253e9f52458bc567ac4f
Apostolos Kritikos <akritiko@gmail.com>
2015-04-15 00:33:28 +0300

    Spring Cleaning :) and some additions

--- js/crime.skg.js
+++ js/crime.skg.js
@@ -1,10 +1,60 @@
+/* ------------------------------------------ *
+ |  Crime In Thessaloniki Javascript Library  |
+ * ------------------------------------------ */
+
 /**
- *  Crime In Thessaloniki Javascript Library
+ * Returns an array with all the available years.
  */
 
 function get_years() {
     return Object.keys(data.groupBy("year"));
 }
+
+/**
+ * Returns a double array with all crime categories sorted alphabetically
+ * by name and matched with their crimeid value.
+ *
+ * e.g. [ ["Rape, "4"], [ ... ], ... ]
+ */
+function get_crime_types() {
+    crimes = Array();
+    label = "";
+    crime_types = data.findAll({year: "2014"}).sortBy("level0");
+    crime_types.forEach(function(item) {
+        label = item["level0"];
+        if(item["level1"] != "-")
+            label = label + " / " + item["level1"];
+        if(item["level2"] != "-")
+            label = label + " / " + item["level2"];
+        crimes.push([label.trim(),item["crimeid"]]);
+    });
+    return crimes;
+}
+
+function get_max_year_for_crime(crimeid) {
+
+}
+
+
+/** crime, max_year, min_year, avg */
+function print_crime_table() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function get_years_menu() {
     years = get_years();
@@ -47,8 +97,3 @@ function html_output_crime_by_year(year) {
     document.write(crime_by_year_body(year));
     socument.write("</table>");
 }
-
-/**
- * Other Code
- */
-
